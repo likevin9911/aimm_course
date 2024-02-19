b3d048566feee8b6f65044e8c8a24bc74a9e1da9
likevin9911 <kgli@mtu.edu>
2024-02-19 13:43:43 -0800

    Initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,18 @@
+# CMake generated Testfile for 
+# Source directory: /home/nope/aimm_ws/src
+# Build directory: /home/nope/aimm_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("aimm")
+subdirs("vrx/usv_msgs")
+subdirs("vrx/wave_gazebo_plugins")
+subdirs("vrx/usv_gazebo_plugins")
+subdirs("vrx/wamv_description")
+subdirs("vrx/wamv_gazebo")
+subdirs("vrx/cora_description")
+subdirs("vrx/wave_gazebo")
+subdirs("vrx/vrx_gazebo")
+subdirs("vrx/vorc_gazebo")
+subdirs("vrx/vrx_2019")
